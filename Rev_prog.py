89bc5d0e299c2779b58d2361995b9c584c6b3ec1
iysh <yashstudy02@gmail.com>
2023-02-24 15:34:45 +0530

    use a funct to oraganize the code

--- Rev_prog.py
+++ Rev_prog.py
@@ -6,18 +6,26 @@ first_number = int(input("enter first number : "))
 operator = input("Enter Operator(+,-,*,?) : ")
 second_number = int(input("Enter Second Number : "))
 
-if operator == "+":
-    add = first_number + second_number
-    print(add)
-elif operator == "-":
-    sub = first_number - second_number
-    print(sub)
-elif operator == "*":
-    mul = first_number * second_number
-    print(mul)
-elif operator == "/":
-    div = first_number / second_number
-    print(div)
-else:
-    print("Please provide correct input!")
 
+
+
+
+def calculator(first_number,second_number,operator):
+    if operator == "+":
+        add = first_number + second_number
+        print(add)
+    elif operator == "-":
+        sub = first_number - second_number
+        print(sub)
+    elif operator == "*":
+        mul = first_number * second_number
+        print(mul)
+    elif operator == "/":
+        div = first_number / second_number
+        print(div)
+    else:
+        print("Please provide correct input!")
+
+
+
+calculator(first_number,second_number,operator);
