42afb3f931038215c309c36805ead843be344028
iysh <yashstudy02@gmail.com>
2023-02-24 15:29:46 +0530

    built using simple if else

--- Rev_prog.py
+++ Rev_prog.py
@@ -1,1 +1,23 @@
-print("Hello World")
+#let's start building a calculator
+
+#first we will define 3 variables
+
+first_number = int(input("enter first number : "))
+operator = input("Enter Operator(+,-,*,?) : ")
+second_number = int(input("Enter Second Number : "))
+
+if operator == "+":
+    add = first_number + second_number
+    print(add)
+elif operator == "-":
+    sub = first_number - second_number
+    print(sub)
+elif operator == "*":
+    mul = first_number * second_number
+    print(mul)
+elif operator == "/":
+    div = first_number / second_number
+    print(div)
+else:
+    print("Please provide correct input!")
+
